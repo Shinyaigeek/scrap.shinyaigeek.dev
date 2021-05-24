c0603000e64ba563246b9d6e8a95d638953ec6ed
shinyaigeek <philispaxil@gmail.com>
2021-05-24 23:15:54 +0900

    Feature: add async endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, Rocket!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[get("/async")]
+async fn async_hello() -> &'static str {
+    rocket::tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    "Hello, Rocket! I send this message 1s later"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, async_hello])
 }
