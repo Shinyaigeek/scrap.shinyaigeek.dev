f1c10b3043fd25bf69c1205778c324b7f766016e
shinyaigeek <philispaxil@gmail.com>
2021-06-06 19:52:17 +0900

    Feature: fix http client

--- src/util/http_client.rs
+++ src/util/http_client.rs
@@ -1,20 +1,23 @@
-use hyper::{Client, Body, Uri};
-use hyper::client::{HttpConnector};
 use hyper::body::to_bytes;
+use hyper::client::HttpConnector;
+use hyper::{Body, Client, Method, Request, Uri};
+use hyper_tls::HttpsConnector;
+use std::collections::HashMap;
 
 pub struct HttpClient {
-    client: Client<HttpConnector, Body>
+    client: Client<HttpsConnector<HttpConnector>, Body>,
 }
 
 pub struct HttpResponse {
-    pub body: String
+    pub body: String,
 }
 
 impl HttpClient {
     pub fn new() -> Self {
+        let https = HttpsConnector::new();
         return Self {
-            client: Client::new()
-        }
+            client: Client::builder().build::<_, hyper::Body>(https),
+        };
     }
 
     pub async fn get(self, url: &str) -> HttpResponse {
@@ -24,8 +27,39 @@ impl HttpClient {
         let body = to_bytes(res).await;
         let body = body.unwrap();
         let body = String::from_utf8(body.to_vec()).unwrap();
-        HttpResponse {
+        HttpResponse { body }
+    }
+
+    pub async fn post(self, url: &str, payload: HashMap<String, String>) -> HttpResponse {
+        let uri = url.parse::<Uri>().unwrap();
+        let request_body = {
+            let mut body = String::from("{");
+            for (key, value) in payload {
+                body.push_str("\"");
+                body.push_str(&key);
+                body.push_str("\"");
+                body.push_str(":");
+                body.push_str("\"");
+                body.push_str(&value);
+                body.push_str("\"");
+                // body.push_str(",");
+            }
+            body.push_str("}");
+
             body
-        }
+        };
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri(url)
+            .header("content-type", "application/json")
+            .body(Body::from(request_body))
+            .expect("request builder");
+
+        let res = self.client.request(req).await;
+        let mut res = res.unwrap().into_body();
+        let body = to_bytes(res).await;
+        let body = body.unwrap();
+        let body = String::from_utf8(body.to_vec()).unwrap();
+        HttpResponse { body }
     }
 }
