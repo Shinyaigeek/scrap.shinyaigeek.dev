64f36392fcac95f8bd7966ccc890e401fcd9f42b
shinyaigeek <philispaxil@gmail.com>
2021-06-06 19:52:00 +0900

    Feature: add reader with sql

--- src/db/_repositories/users/read.rs
+++ src/db/_repositories/users/read.rs
@@ -5,5 +5,9 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
 pub fn read(gh_user_id: String, connection: PgConnection) -> Option<User> {
-    users.find(gh_user_id).first(&connection)
+    let res: Result<User, diesel::result::Error> = users::dsl::users.find(gh_user_id).first(&connection);
+    match res {
+        Ok(res) => Some(res),
+        Err(_) => None
+    }
 }
