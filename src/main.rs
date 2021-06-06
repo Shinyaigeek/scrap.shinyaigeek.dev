c4a22a90bf2aebc8fdfefffdc454d36f9b033025
shinyaigeek <philispaxil@gmail.com>
2021-06-06 15:12:43 +0900

    Feature: get auth from request header

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, HttpRequest};
 
 #[macro_use]
 extern crate diesel;
@@ -15,15 +15,25 @@ use std::env;
 use crate::db::_repositories::threads::create::{create, NewThread};
 use crate::db::_repositories::threads::read::reads;
 use crate::db::connection::establish::establish_connection;
+use crate::routes::users::signin;
 
 mod db;
+mod routes;
 
-// async fn create_threads() -> impl Responder {
-//     dotenv().ok();
+fn get_auth_from_header(req: HttpRequest) -> Option<String> {
+    let auth = req.headers().get("Authorization");
+    let auth = match auth {
+        Some(a) => Some(a.to_str().unwrap().replace("Bearer ", "")),
+        None => None
+    };
 
-//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-//     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-// }
+    auth
+}
+
+async fn dispatch_signin(req: HttpRequest) -> impl Responder {
+    println!("REQ: {:?}", req.headers().get("accept"));
+    "asdf"
+}
 
 async fn pos() -> impl Responder {
     let connection = establish_connection();
@@ -59,8 +69,23 @@ async fn main() -> std::io::Result<()> {
             .route("/async", web::get().to(async_hello))
             .route("/threads", web::get().to(asdf))
             .route("/post_threads", web::get().to(pos))
+            .route("/signin", web::get().to(dispatch_signin))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http, test};
+
+    #[actix_rt::test]
+    async fn get_auth() {
+        let req = test::TestRequest::with_header("content-type", "text/plain").to_http_request();
+        assert_eq!(get_auth_from_header(req), None);
+        let req = test::TestRequest::with_header("Authorization", "Bearer asdf").to_http_request();
+        assert_eq!(get_auth_from_header(req), Some("asdf".to_string()));
+    }
 }
