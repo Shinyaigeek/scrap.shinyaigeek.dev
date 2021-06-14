b218bbb9a4bfa4cb55130fed74f9265ebb165ce5
shinyaigeek <philispaxil@gmail.com>
2021-06-14 20:28:56 +0900

    Feature: add application for create comments

---
+++ src/db/applications/comments/create.rs
@@ -0,0 +1,20 @@
+use super::super::super::_models::comments::Comment;
+use super::super::super::_repositories::comments::create::{create, NewComment};
+use super::super::super::connection::establish::DbConnection;
+use diesel::result::Error;
+
+pub fn create_comment(
+    content: String,
+    author: String,
+    thread: i32,
+    connection: DbConnection,
+) -> Result<Comment, Error> {
+    create(
+        NewComment {
+            content,
+            author,
+            thread
+        },
+        connection,
+    )
+}
--- src/db/applications/mod.rs
+++ src/db/applications/mod.rs
@@ -1,3 +1,4 @@
 pub mod threads;
 pub mod users;
 pub mod utils;
+pub mod comments;
