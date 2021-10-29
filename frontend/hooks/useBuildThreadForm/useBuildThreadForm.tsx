ecb95ea904da487970d4a22bc41e2c78f20aa8ce
Shinyaigeek <Philispaxil@gmail.com>
2021-10-29 23:14:59 +0900

    Feature: add Thread scleme for hooks

--- frontend/hooks/useBuildThreadForm/useBuildThreadForm.tsx
+++ frontend/hooks/useBuildThreadForm/useBuildThreadForm.tsx
@@ -1,13 +1,20 @@
 import { useCallback, useMemo } from "react";
 import { useForm } from "react-hook-form";
 
+interface ThreadForm {
+  title: string;
+  slug: string;
+  content: string;
+  published: boolean;
+}
+
 export const useBuildThreadForm = function () {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm();
+  } = useForm<ThreadForm>();
 
   const onSubmit = useCallback(
     handleSubmit((data) => {
@@ -19,12 +26,10 @@ export const useBuildThreadForm = function () {
   const Form = useCallback(() => {
     return (
       <form onSubmit={onSubmit}>
-        <input {...register("firstName")} />
-        <select {...register("gender")}>
-          <option value="female">female</option>
-          <option value="male">male</option>
-          <option value="other">other</option>
-        </select>
+        <input {...register("title")} />
+        <input {...register("slug")} />
+        <input type="checkbox" {...register("published")} />
+        <textarea {...register("content")} />
         <input type="submit" />
       </form>
     );
