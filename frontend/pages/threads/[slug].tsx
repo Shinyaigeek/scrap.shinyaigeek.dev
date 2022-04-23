4419ead41758f8b9426a541ae61f584ee09a2907
Shinyaigeek <Philispaxil@gmail.com>
2022-04-23 11:51:39 +0900

    Feature: show comment on thread page

--- frontend/pages/threads/[slug].tsx
+++ frontend/pages/threads/[slug].tsx
@@ -1,5 +1,5 @@
 import { useAuthUser } from "../../hooks/useAuthUser/useAuthUser";
-import { useBuildCommentForm } from '../../hooks/useBuildCommentForm/useBuildCommentForm';
+import { useBuildCommentForm } from "../../hooks/useBuildCommentForm/useBuildCommentForm";
 interface Props {
   thread: {
     title: string;
@@ -7,16 +7,24 @@ interface Props {
     is_open: boolean;
     content: string;
     id: number;
+    comments: {
+      content: string;
+      author: string;
+      thread: number;
+    }[];
   };
 }
 
-function Thread({ thread }: Props) {
+function Thread({ thread, }: Props) {
   const { title, slug, content } = thread;
   const [Form] = useBuildCommentForm(thread.id);
   return (
     <>
       <h1>{title}</h1>
       <p>{content}</p>
+
+      <h2>comments</h2>
+      {thread.comments.map((comment) => (<div>{comment.content}</div>))}
 
       <p>write comment</p>
       <Form />
@@ -25,7 +33,9 @@ function Thread({ thread }: Props) {
 }
 
 export const getServerSideProps = async (ctx) => {
-  const res = await fetch(`http://localhost:8080/threads/${ctx.query.slug}`);
+  const res = await fetch(
+    `http://localhost:3000/api/thread/read?slug=${ctx.query.slug}`
+  );
   const thread = await res.json();
   return {
     props: {
