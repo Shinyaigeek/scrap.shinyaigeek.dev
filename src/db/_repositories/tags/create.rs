db98e7e5c765df90277dcbfae852f1288a94a748
Shinyaigeek <Philispaxil@gmail.com>
2022-04-24 14:19:17 +0900

    Feature: add create tag repository

---
+++ src/db/_repositories/tags/create.rs
@@ -0,0 +1,20 @@
+use super::super::super::_models::tags::Tag;
+use super::super::super::_schemas::tags::tags;
+use super::super::super::connection::establish::DbConnection;
+use diesel;
+use diesel::prelude::*;
+use diesel::result::Error;
+use std::time::SystemTime;
+
+#[derive(Insertable)]
+#[table_name = "tags"]
+pub struct NewTag {
+    pub tag: String,
+    pub icon: String
+}
+
+pub fn create(tag: NewTag, connection: DbConnection) -> Result<Tag, Error> {
+    diesel::insert_into(tags::table)
+        .values(&tag)
+        .get_result(&connection)
+}
