e2873dd2c236f94fb2fce52b4495105ac1a20de7
Shinyaigeek <Philispaxil@gmail.com>
2022-04-29 14:02:04 +0900

    Feature: impolement read tag application

--- src/db/applications/tags/read.rs
+++ src/db/applications/tags/read.rs
@@ -1,8 +1,12 @@
-use super::super::super::_models::comments::Comment;
-use super::super::super::_repositories::comments::read::read_comments as _read_comments;
+use super::super::super::_models::tags::Tag;
+use super::super::super::_repositories::tags::read::{read_tag as _read_tag, read_all_tags as _read_all_tags};
 use super::super::super::connection::establish::DbConnection;
 use diesel::result::Error;
 
-pub fn read_comments(thread_id: i32, connection: DbConnection) -> Result<Vec<Comment>, Error> {
-    _read_comments(thread_id, connection)
+pub fn read_tag(tag_id: i32, connection: DbConnection) -> Result<Vec<Tag>, Error> {
+    _read_tag(tag_id, connection)
+}
+
+pub fn read_all_tags(connection: DbConnection) -> Result<Vec<Tag>, Error> {
+    _read_all_tags(connection)
 }
